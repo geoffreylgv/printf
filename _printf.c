0d6d5ccb3ddc049c3459e7bd0458c5028969532b
geoffreylgv <evanomarkez@gmail.com>
2023-07-22 23:17:33 +0000

    create _printf.c empty

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _printf - The standard print formatted of the original c
+ * @format: String character, the format string is
+ * composed of zero or more directives, see: man 3 printf
+ *
+ * Return: Always 0 on success
+ */
+int _printf(const char *format, ...)
+{
+
+}
+
