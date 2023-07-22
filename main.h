32ee3e130ec46131907fcb921f5b73473335dd88
geoffreylgv <evanomarkez@gmail.com>
2023-07-22 23:14:16 +0000

    add main.h and litlle proto

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* headers call go here */
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+
+/* prototypes go here */
+int _putchar(char c);
+
+
+#endif
