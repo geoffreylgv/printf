beab1969d936667fdf05c0ce9d6a9340abe34c36
geoffreylgv <geoffreylogovi2@gmail.com>
2023-07-24 09:04:36 -0700

    _printf decomposed to handle specific format

--- _printf.c
+++ _printf.c
@@ -1,1 +1,39 @@
+#include "main.h"
+
+/**
+ * _printf - The standard print formatted of the original c
+ * @format: String character, the format string is
+ * composed of zero or more directives, see: man 3 printf
+ *
+ * Return: Count as integer
+ */
+int _printf(const char *format, ...)
+{
+	int count;
+	va_list args;
+
+	count = 0;
+
+	va_start(args, format);
+
+	if (!format || !format[0])
+		return (-1);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format_navigation(format, args, &count);
+			format++;
+		}
+		else
+		{
+			count += _putchar(*format);
+			format++;
+		}
+	}
+
+	va_end(args);
+	return (count);
+}
 
