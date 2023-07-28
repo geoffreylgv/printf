7c79bcd5480e76b0779e4a6d2ef8b3ae3f36e8e4
geoffreylgv <geoffreylogovi2@gmail.com>
2023-07-28 07:20:08 -0700

    doc: update README, add documentation

--- main.c
+++ main.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <limits.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - Entry point test function
  *
- * Return: 0 on success, error code otherwise
+ * Return: Success
  */
 int main(void)
 {
-	long int ll = INT_MAX;
-
-	ll *= 2;
-
-	_printf("There is %X bytes in %X KB\n", 1024, 1);
-	_printf("there is %x bytes in %x KB\n", 1024, 1);
-	_printf("%x + %x = %x\n", INT_MAX, INT_MAX, ll );
-	_printf("%X + %X = %X\n", INT_MAX, INT_MAX, ll);
-	_printf("%u == %o == %x == %X\n", 1024, 1024, 1024, 1024);
-	_printf("uuoxxX%xuoXo\n", 1024);
+	int len, len2;
+	len = _printf("%d - %d = %d\n", 1024, 2048, -1024);
+	len2 = printf("%d - %d = %d\n", 1024, 2048, -1024);
+	_printf("len = %d\n", len);
+	printf("len2 = %d\n", len2);
+	_printf("______Binary________\n");
+	_printf("%b\n", 98);
+	printf("%b\n", 98);
+	_printf("\n______________END_____________\n");
 	return (0);
 }
+
